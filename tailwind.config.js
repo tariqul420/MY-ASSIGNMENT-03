5bee81d5a89f48caccded224ccd08ed66fd89554
tariqul420 <tariqul.islam.programmer@gmail.com>
2024-08-21 22:45:21 +0600

    COMPLITE HEADING SECTION

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    fontFamily:{
+      'manrope': ['Manrope', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        "green": "#ABEF5F",
+        "green-2": "#758B5F",
+        "heading": "#212121",
+        "heading-2": "#494949",
+        "heading-d": "#fff",
+        "paragraph-1": "#494949",
+        "paragraph-2": "#737373",
+        "paragraph-d": "#ffffffa6",
+      },
+    },
+  },
+  plugins: [
+    require('daisyui'),
+    require('tailwind-scrollbar'),
+  ],
+  darkMode: "class",
+}
