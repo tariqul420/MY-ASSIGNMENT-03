d5cac9f8a097f03546432e5a0c7781c3225e568b
tariqul420 <tariqul.islam.programmer@gmail.com>
2024-08-23 21:50:18 +0600

    Finish my assignment

--- script.js
+++ script.js
@@ -8,40 +8,40 @@ const systemTheme = window.matchMedia("(prefers-color-scheme :dark)").matches;
 
 // Icon Toggling
 const iconToggle = () => {
-    moonIcon.classList.toggle("display-none");
-    sunIcon.classList.toggle("display-none");
+  moonIcon.classList.toggle("display-none");
+  sunIcon.classList.toggle("display-none");
 };
 
 // Initial Theme Check
 const themeCheck = () => {
-    if (userTheme === "dark" || (!userTheme && systemTheme)) {
-        document.documentElement.classList.add("dark");
-        moonIcon.classList.add("display-none");
-        return;
-    }
-    sunIcon.classList.add("display-none");
+  if (userTheme === "dark" || (!userTheme && systemTheme)) {
+    document.documentElement.classList.add("dark");
+    moonIcon.classList.add("display-none");
+    return;
+  }
+  sunIcon.classList.add("display-none");
 };
 
 // Manual Theme switch
 const themeSwitch = () => {
-    if (document.documentElement.classList.contains("dark")) {
-        document.documentElement.classList.remove("dark");
-        localStorage.setItem("theme", "light");
-        iconToggle();
-        return;
-    }
-    document.documentElement.classList.add("dark");
-    localStorage.setItem("theme", "dark");
+  if (document.documentElement.classList.contains("dark")) {
+    document.documentElement.classList.remove("dark");
+    localStorage.setItem("theme", "light");
     iconToggle();
+    return;
+  }
+  document.documentElement.classList.add("dark");
+  localStorage.setItem("theme", "dark");
+  iconToggle();
 };
 
 // Call Theme switch on Click Button
 sunIcon.addEventListener("click", () => {
-    themeSwitch();
+  themeSwitch();
 });
 
 moonIcon.addEventListener("click", () => {
-    themeSwitch();
+  themeSwitch();
 });
 
 // Invoke Theme Check on Initial lode
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,17 @@ module.exports = {
     },
     extend: {
       colors: {
-        "green": "#ABEF5F",
-        "heading": "#212121",
+        green: "#ABEF5F",
+        heading: "#212121",
         "heading-2": "#494949",
         "heading-d": "#fff",
         "paragraph-1": "#494949",
         "paragraph-2": "#737373",
         "paragraph-d": "#ffffffa6",
+        "aqua-marine": "#44deb7",
       },
       backgroundImage: {
-        "connect": "url('/assets/bg.jpg')",
+        connect: "url('/assets/bg.jpg')",
       },
     },
   },
