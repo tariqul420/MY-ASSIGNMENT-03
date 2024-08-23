88fe3781b08629a1129631e246721b7e4cb7cee2
tariqul420 <tariqul.islam.programmer@gmail.com>
2024-08-23 10:07:43 +0600

    add Let’s Connect section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,27 +2,24 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    fontFamily:{
-      'manrope': ['Manrope', 'sans-serif']
+    fontFamily: {
+      manrope: ["Manrope", "sans-serif"],
     },
     extend: {
       colors: {
-        "green": "#ABEF5F",
-        "green-2": "#758B5F",
-        "heading": "#212121",
+        green: "#ABEF5F",
+        heading: "#212121",
         "heading-2": "#494949",
         "heading-d": "#fff",
         "paragraph-1": "#494949",
         "paragraph-2": "#737373",
         "paragraph-d": "#ffffffa6",
       },
-      aspectRatio: {
-        'img': '9 / 5',
+      backgroundImage: {
+        connect: "url('/assets/bg.jpg')",
       },
     },
   },
-  plugins: [
-    require('daisyui'),
-  ],
+  plugins: [require("daisyui")],
   darkMode: "class",
-}
+};
